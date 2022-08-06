adcce535d92a6188246c83ae7e9be6ed49cb8735
Clara Solomon <clarasolomon@outlook.com>
2022-08-06 12:49:40 -0500

    Update LeapYearCalculator.cpp

--- Leap Year Calculator C++/LeapYearCalculator.cpp
+++ Leap Year Calculator C++/LeapYearCalculator.cpp
@@ -9,11 +9,6 @@ int year; //inserted by the user
 bool leapTrue = true; // if year is leap
 bool leapFalse = false; //if year isnt leap
 
-
-cout<<"Welcome to the leap year calculator."<<endl;
-cout<<"Please Enter a Year"<<endl;
-cin>>year;
-
 //Calculation if a year is evenly divisible by 4 (leap year)
 if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
     cout<<"The year "<<year<<" is a leap year."<<endl;//leapTrue<<endl;
@@ -28,6 +23,12 @@ if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
     else{
         cout<<"The year is not a leap year."<<endl;
     }
+//Output whether input year is a leap year or not    
+cout<<"Welcome to the leap year calculator."<<endl;
+cout<<"Please Enter a Year"<<endl;
+cin>>year;
+
+
     
 return 0;
 } 
